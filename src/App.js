236610841bc177cc38a494892e85a0b453818020
Ashvita Ramanan <r.ashvita.mec22@itbhu.ac.in>
2023-09-18 23:37:59 +0530

    3

--- src/App.js
+++ src/App.js
@@ -1,33 +1,9 @@
 import Header from './components/header';
 import './App.css';
-import Navbar from './components/navbar';
-import Abtteam from './components/AboutTeam';
-import Abti3f from './components/AboutI3f';
-import Abtteam1 from './components/AboutTeam1';
-import Rabi from './components/R-ABI';
-import Cisco from './components/Cisco';
-import Ecell from './components/ecell';
-import Nl from './components/newsletter';
-import Services from './components/Services';
-import Sc from './components/SatelliteCentre';
-<link href='https://fonts.googleapis.com/css?family=Inter' rel='stylesheet'></link>
 
 function App() {
   return (
-    <div>
-      <Sc />
-      {/* <Tr /> */}
-      {/* <Services /> */}
-      {/* <Nl /> */}
-      {/* <Ecell /> */}
-      {/* <Cisco /> */}
-      {/* <Rabi /> */}
-    {/* <Header /> */}
-    {/* <Navbar /> */}
-    {/* <Abtteam /> */}
-    {/* <Abtteam1 /> */}
-    {/* <Abti3f /> */}
-    </div>
+    <Header />
   )
 }
 
