a66f16eb1305cf5f45fda33c9c27407b6e706dfd
Ashvita-6 <127018079+Ashvita-6@users.noreply.github.com>
2023-09-19 14:54:52 +0530

    Update SatelliteCentre.jsx

--- src/components/SatelliteCentre.jsx
+++ src/components/SatelliteCentre.jsx
@@ -4,18 +4,39 @@ import './SatelliteCentre.css';
 function Sc(){
     return(
         <div>
-        <h1>Satellite Centre</h1>
+        <div>
+        <h1 className="sch">    
+            <div class="arrow">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            Satellite Centre
+            <div class="arrow1">
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+        </h1>
+        </div>
+        <div className="p1">
         <p>The main establishment of this Centre is at IIT (BHU) and further its Satellite Centres are being developed at Singrauli for extending the benefit of this initiative operational as well as CSR and welfare areas of NCL. As per directive of Ministry of Corporate Affairs, NCL is supporting NIIC under its CSR from last 2 years. The main objectives of this Centre are as follows:</p>
-        <li />To promote entrepreneurship among students and society
-        <li />To create support system for Innovation-based enterprises/start-ups
-        <li />To encourage technology transfer and commercialization
-        <li />To sustain job creation and support “Make in India” Model
-        <li />To forge a close linkage between new business & academia    R & D
-        <li />To provide R & D support to SMEs of the region and business incubators
-        <h2>Key initiatives taken by this Centre in last 2 years are as follows:</h2>
-        <li />Establishment of Khadi and Handloom Centre at Semuar village as Satellite Centre of NIIC
-        <li />Establishment of “Food Tech-Park-Training & Development Centre” as Satellite Centre of NIIC at Birkunia village of Singrauli
-        <li />Establishment of Drone Manufacturing and Rapid Prototyping Lab under satellite Centre of NIIC at NCL-HQ, Singrauli
+        </div>
+        <div className="pts1">
+        <li>To promote entrepreneurship among students and society</li>
+        <li>To create support system for Innovation-based enterprises/start-ups</li>
+        <li >To encourage technology transfer and commercialization</li>
+        <li >To sustain job creation and support “Make in India” Model</li>
+        <li >To forge a close linkage between new business & academia    R & D</li>
+        <li >To provide R & D support to SMEs of the region and business incubators</li>
+        </div>
+        <h2 className="ki">Key initiatives taken by this Centre in last 2 years are as follows:</h2>    
+        <div className="pts1">
+        <li >Establishment of Khadi and Handloom Centre at Semuar village as Satellite Centre of NIIC</li>
+        <li >Establishment of “Food Tech-Park-Training & Development Centre” as Satellite Centre of NIIC at Birkunia village of Singrauli</li>
+        <li >Establishment of Drone Manufacturing and Rapid Prototyping Lab under satellite Centre of NIIC at NCL-HQ, Singrauli</li>    
+        </div>
+        
         </div>
     )
 }
