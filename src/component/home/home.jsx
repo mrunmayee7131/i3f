14e520fdf0b0a9394676821605d067b5e43a0135
Ashvita Ramanan <r.ashvita.mec22@itbhu.ac.in>
2024-04-06 04:02:04 +0530

    hero text

--- src/component/home/home.jsx
+++ src/component/home/home.jsx
@@ -59,8 +59,8 @@ return(
 <section class="bg-white  mt:36 h-1200 custom:h-350">
     <div class="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-36 lg:grid-cols-12">
         <div class="mr-auto place-self-center lg:col-span-7 ">
-            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black ">Innovation Incubation and Ideation Center</h1>
-            <p class="max-w-2xl mb-6 ml-12 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"> I3f Foundation , Indian Institute of Technology (BHU) Varanasi</p>
+            <h1 class="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-black ">Innovation Incubation and Ideation Foundation</h1>
+            <p class="max-w-2xl mb-6 ml-12 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400"> I3-f Foundation , Indian Institute of Technology (BHU) Varanasi</p>
             <p class="inline-flex ml-12 items-center justify-center px-5 py-3 mr-3 text-base border bg-gray-400 font-medium text-center text-black rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900">
                 Queries?
                 <svg class="w-5 h-5 ml-2 -mr-1" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
