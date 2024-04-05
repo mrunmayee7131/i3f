03e71a226340a229d7b0adbe07f9f9317375de74
name <you@example.com>
2024-04-06 04:05:23 +0530

    cards and count changes

--- src/component/carousel/carousel.jsx
+++ src/component/carousel/carousel.jsx
@@ -4,22 +4,22 @@ import React from 'react'
 import IMG1 from '../carousel/assets/cisco.webp';
 import IMG2 from '../carousel/assets/Dhruv.webp';
 import { Link } from 'react-router-dom';
-
+import Training from "../components/assets/Animation - 1712353973184.gif"
 
 
 function carousel() {  
   const data =[
     {
       IMGUrl:IMG1,
-      Details:" Workshop to guide graduate from product MVP to business soft launch..."
+      Details:"Workshop to guide graduate from product MVP to business soft launch.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip lorem ipsum nisi ut."
     },
     {
       IMGUrl:IMG2,
-      Details:" Workshop to guide graduate from product MVP to business soft launch..."
+      Details:"Workshop to guide graduate from product MVP to business soft launch.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip lorem ipsum nisi ut."
     },
     {
       IMGUrl:IMG1,
-      Details:" Workshop to guide graduate from product MVP to business soft launch..."
+      Details:"Workshop to guide graduate from product MVP to business soft launch.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip lorem ipsum nisi ut."
     },
     // {
     //   IMGUrl:IMG1,
@@ -47,13 +47,13 @@ function carousel() {
      columnCount = 2;
    }
     return (
-      <div  class="py-12">
+      <div  class="py-12 bg-gray-100">
       <div class="xl:container m-auto px-6 text-gray-600 xl:px-1">
-      <div class="mb-12 space-y-2 text-center">
+      <div class="mb-12 space-y-2 text-center ">
       <h2 class="text-3xl font-bold text-gray-800 md:text-4xl dark:text-fuchsia-800">Current Happenings</h2>
       
     </div>
-    <div className={`grid gap-12 ${singleCard ? 'justify-center' : ''} ${singleCard ? 'w-full' : `md:grid-cols-${columnCount}`}`}>
+    <div className={`flex flex-row justify-around align-center p-[150px] pt-0 pb-0`}>
 
 
 
@@ -70,9 +70,9 @@ function carousel() {
           </div> */}
 
 {data.map((element, index) => (
-  <div key={index} className={`group p-6 sm:p-8 rounded-3xl bg-white border border-gray-100 dark:shadow-none dark:border-gray-700 dark:bg-white-200 ${cardHeight}`} >
-    <div className="relative overflow-hidden rounded-xl">
-      <img
+  <div key={index} className={` p-6 sm:p-8  bg-white    dark:border-gray-700 dark:bg-white-200 ${cardHeight}  w-[350px] shadow-xl`} >
+    <div className="relative overflow-hidden flex justify-center">
+      {/* <img
         src={element.IMGUrl}
         alt="art cover"
         loading="lazy"
@@ -85,13 +85,17 @@ function carousel() {
           e.target.src = 'https://via.placeholder.com/1000x667'; // Placeholder image or default image URL
 
         }}
-      />
+      /> */}
+      <img src={Training} height={200}/>
     </div>
     <div className="mt-6 relative">
-      <h3 className="text-xl font-semibold text-gray-500 dark:gray-900 pb-8">
-        {element.Details}<Link className='text-lg font-medium text-blue-500 hover:text-blue-900'target='_parent' to='/Blog'>Read more</Link>
+      <h3 className="text-md font-light text-gray-500 dark:gray-800 pb-8 text-center">
+        {element.Details}
       </h3>
     </div>
+    <div className='flex justify-center'>
+      <button className='bg-gray-800 w-[150px] h-[40px] rounded-md'><Link className='text-lg font-medium text-white 'target='_parent' to='/Blog'>Read more</Link></button>
+      </div>
   </div>
 ))}
 
--- src/component/count/count.jsx
+++ src/component/count/count.jsx
@@ -1,6 +1,10 @@
 import React,{useEffect,useState} from "react"
 import styles from "./count.module.css"
 import CountUpWithVisibility from '../components/rvc';
+import Company from "../../component/components/assets/company__1_-removebg-preview.png";
+import Business from "../components/assets/business (3).png"
+import Funding from "../components/assets/png/001-funding.png"
+import Patent from "../components/assets/patent.png"
 function Count(){
     return(
         <div className={styles.countbody}>
@@ -9,41 +13,53 @@ function Count(){
             </div>
          <div className={styles.outerdiv}>
             <div >
-            <div >
+            <div  className="flex flex-col items-center">
+                <div>
+                <img src={Company} width={100} className="mix-blend-screen"/>
+                </div>
+            
             <div  >
-                <div ><h1  className={styles.countuptext}>  <CountUpWithVisibility start={0} end={130} duration={3} /></h1></div>
+                <div ><h1  className={styles.countuptext}>130 +</h1></div>
+              
             </div>
+            <p className="text-xl font-light text-gray-300">Trained Start ups</p>
             </div>
-            <p className="text-xl font-semibold text-gray-500">Trained Start ups</p>
+          
             </div>
 
 
             <div >
-            <div >
+            <div className="flex flex-col items-center" >
+            <img src={Business} width={100} className="mix-blend-screen"/>
             <div  >
-                <div ><h1  className={styles.countuptext}>  <CountUpWithVisibility start={0} end={53} duration={3} /></h1></div>
+                <div ><h1  className={styles.countuptext}>53 +</h1></div>
             </div>
+            <p className="text-xl font-light text-gray-300">Incubated Start Ups</p>
             </div>
-            <p className="text-xl font-semibold text-gray-500">Incubated Start Ups</p>
+         
             </div>
 
 
             <div >
-            <div >
+            <div className="flex flex-col items-center">
+            <img src={Funding} width={100} className="mix-blend-screen"/>
             <div  >
-                <div ><h1  className={styles.countuptext}>  <CountUpWithVisibility start={0} end={230} duration={3} /></h1></div>
+                <div ><h1  className={styles.countuptext}>230 +</h1></div>
             </div>
+            <p className="text-xl font-light text-gray-300">Lakhs Fund Supported</p>
             </div>
-            <p className="text-xl font-semibold text-gray-500">Lakhs Fund Supported</p>
+          
             </div>
 
             <div >
+            <div className="flex flex-col items-center">
+            <img src={Patent} width={100} className="mix-blend-screen"/>
             <div >
-            <div >
-                <div ><h1  className={styles.countuptext1}>  <CountUpWithVisibility start={0} end={19} duration={3} /></h1></div>
+                <div ><h1  className={styles.countuptext1}> 19 +</h1></div>
             </div>
+            <p className="text-xl font-light text-gray-300">Patentable technologies</p>
             </div>
-            <p className="text-xl font-semibold text-gray-500">Patentable technologies</p>
+           
             </div>
          </div>
         </div>
